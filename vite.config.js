4b4902d54a7b802468ebfcf6b8c2a0d3447c5554
Kate Marchuk <pion3219@gmail.com>
2024-11-19 22:28:20 +0200

    fixed deployment

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vue_dynamic-list-of-posts/',
+  base: '/vue_dynamic-list-of-posts',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
